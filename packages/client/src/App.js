7faff9f7db69d983dcdf4f24473cabf527eda802
André Murilo <lifercode@gmail.com>
2020-08-08 01:18:18 -0300

    set monorepo actions

---
+++ packages/client/src/App.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect, useCallback } from 'react'
+import axios from 'axios'
+
+const App = () => {
+  const [isLoading, setLoading] = useState(true)
+  const [text, setText] = useState('')
+
+  const fetchData = useCallback(async () => {
+    try {
+      const result = await axios.get('http://localhost:5000/')
+      console.log({result})
+      setText(result.data.text)
+      setLoading(false)
+    } catch (error) {
+      console.log(error)
+      setLoading(false)
+    }
+  }, [])
+
+  useEffect(() => {
+    fetchData()
+  }, [fetchData])
+
+  return (
+    <div>
+      <p>{isLoading ? 'Loading...' : text}</p>
+    </div>
+  );
+}
+
+export default App
---
+++ packages/server/src/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+  return res.json({ text: 'hello world from API' })
+})
+
+app.listen(5000, () => console.log('run on 5000'))
