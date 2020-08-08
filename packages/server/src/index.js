074cdd4e41d589eb4e23addd415512f1ace2affd
André Murilo <46170334+lifercode@users.noreply.github.com>
2020-08-08 03:06:27 -0300

    Update index.js

--- packages/server/src/index.js
+++ packages/server/src/index.js
@@ -3,10 +3,12 @@ const cors = require('cors')
 console.log('hi')
 const app = express()
 
+const PORT = proccess.env.PORT || 5000
+
 app.use(cors())
 
 app.get('/', (req, res) => {
   return res.json({ text: 'hello world from API' })
 })
 
-app.listen(5000, () => console.log('run on 5000'))
+app.listen(PORT, () => console.log(`run on ${PORT}`))
